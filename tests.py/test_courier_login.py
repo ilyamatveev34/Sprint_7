8c98135b4348cffd50295e78c226e09eaba92f87
Ilia Matveev <ilyamatveev599@gmail.com>
2024-08-06 11:38:48 +0700

    add allure report

---
+++ tests.py/test_courier_login.py
@@ -0,0 +1,45 @@
+import requests
+import allure
+import pytest
+from data import Data
+from urls import Urls
+from helpers import create_random_login, create_random_password, create_random_firstname
+
+
+class TestCourierLogin:
+
+    @allure.title('Проверка успешной аутентификации курьера при вводе валидных данных')
+    @allure.description('Проверяются код и тело ответа.')
+    def test_courier_login_success(self):
+        payload = {
+            'login': create_random_login(),
+            'password': create_random_password(),
+            'firstName': create_random_firstname()
+        }
+        requests.post(Urls.URL_courier_create, data=payload)
+        response = requests.post(Urls.URL_courier_login, data=payload)
+        assert response.status_code == 200 and 'id' in response.text
+
+    @allure.title('Проверка получения ошибки аутентификации курьера при вводе невалидных данных')
+    @allure.description('В тест по очереди передаются наборы данных с несуществующим логином или неверным паролем. '
+                        'Проверяются код и тело ответа.')
+    @pytest.mark.parametrize('nonexistent_credentials', [
+        {'login': create_random_login(), 'password': create_random_password()},
+        Data.courier_data_with_wrong_password
+    ])
+    def test_courier_login_nonexistent_data_not_found(self, nonexistent_credentials):
+        response = requests.post(Urls.URL_courier_login, data=nonexistent_credentials)
+        assert response.status_code == 404
+        assert response.json() == {'code': 404, 'message': 'Учетная запись не найдена'}
+
+    @allure.title('Проверка получения ошибки аутентификации курьера с пустым полем логина или пароля')
+    @allure.description('В тест по очереди передаются наборы данных с пустым логином или паролем. '
+                        'Проверяются код и тело ответа.')
+    @pytest.mark.parametrize('empty_credentials', [
+        {'login': '', 'password': create_random_password()},
+        {'login': Data.valid_login, 'password': ''}
+    ])
+    def test_courier_login_empty_credentials_bad_request(self, empty_credentials):
+        response = requests.post(Urls.URL_courier_login, data=empty_credentials)
+        assert response.status_code == 400
+        assert response.json().get('message') == 'Недостаточно данных для входа'
